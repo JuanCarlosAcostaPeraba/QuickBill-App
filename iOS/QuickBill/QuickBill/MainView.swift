631393f0417b7dc7a73d10757ce5514db159d61c
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2025-04-23 12:33:56 +0100

    Add initial project files and structure for QuickBill app

---
+++ iOS/QuickBill/QuickBill/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  QuickBill
+//
+//  Created by Juan Carlos Acosta Perabá on 23/4/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ iOS/QuickBill/QuickBill/QuickBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBillApp.swift
+//  QuickBill
+//
+//  Created by Juan Carlos Acosta Perabá on 22/4/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
