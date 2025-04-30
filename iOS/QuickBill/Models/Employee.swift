e85a1781e22f2475e12ae1035e984f6cab6b2f32
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2025-04-30 15:21:38 +0100

    feat: Add Employee model for employee data representation

---
+++ iOS/QuickBill/Models/Employee.swift
@@ -0,0 +1,16 @@
+//
+//  Employee.swift
+//  QuickBill
+//
+//  Created by Juan Carlos Acosta Perabá on 30/4/25.
+//
+
+import SwiftUI
+
+struct Employee: Identifiable {
+    let id: String
+    let name: String
+    let email: String
+    let phone: String
+    let role: String
+}
