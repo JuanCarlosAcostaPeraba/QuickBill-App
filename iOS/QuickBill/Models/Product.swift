15d2278ccbda0fd755f780971a75afe7e749bdee
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2025-05-01 20:56:56 +0100

    feat: Add Product model for managing product data

---
+++ iOS/QuickBill/Models/Product.swift
@@ -0,0 +1,12 @@
+//
+//  Product.swift
+//  QuickBill
+//
+//  Created by Juan Carlos Acosta Perabá on 1/5/25.
+//
+
+struct Product: Identifiable {
+    let id: String
+    let description: String
+    let unitPrice: Double
+}
