bb79279b21042a58422151ee373927421fa1a7d0
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2025-06-01 12:25:38 +0100

    refactor: Implement middleware for user authentication and redirection

---
+++ Web/src/middleware.ts
@@ -0,0 +1,25 @@
+import { defineMiddleware } from 'astro:middleware'
+import { auth } from '@/firebase/config'
+
+export const onRequest = defineMiddleware((context, next) => {
+	const currentUser = auth.currentUser
+	const { pathname } = context.url
+
+	if (
+		!currentUser &&
+		pathname !== '/login' &&
+		context.request.method === 'GET'
+	) {
+		return context.redirect('/login')
+	}
+
+	if (currentUser) {
+		context.locals.userEmail = currentUser.email
+	}
+
+	if (currentUser && pathname === '/login') {
+		return context.redirect('/')
+	}
+
+	return next()
+})
